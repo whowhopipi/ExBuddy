288913c490d554899dcdaec7cb1d41fbb68427f0
whowhopipi <whowhopipi@163.com>
2017-10-06 12:24:05 +0800

    提交

--- ExBuddy/OrderBotTags/Objects/SpearFish.cs
+++ ExBuddy/OrderBotTags/Objects/SpearFish.cs
@@ -1,10 +1,12 @@
 ﻿namespace ExBuddy.OrderBotTags.Objects
 {
+    using System;
     using Clio.XmlEngine;
     using Interfaces;
+    using Clio.Utilities;
 
     [XmlElement("SpearFish")]
-    public class SpearFish : INamedItem
+    public class SpearFish : IConditionNamedItem
     {
         public override string ToString() { return this.DynamicToString(); }
 
@@ -18,6 +20,26 @@
 
         [XmlAttribute("LocalName")]
         public string LocalName { get; set; }
+
+        [XmlAttribute("Condition")]
+        public string Condition { get; set; }
+
+        public bool ConditionResult
+        {
+            get
+            {
+                if (Condition == null || Condition.Equals(""))
+                    return true;
+
+                if (condition == null)
+                {
+                    condition = ScriptManager.GetCondition(Condition);
+                }
+                return condition();
+            }
+        }
+
+        private Func<bool> condition;
 
         #endregion INamedItem Members
     }
