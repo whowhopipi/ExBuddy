a39712bbb032a14ce88e0b109c3dff9e62ff09ed
whowhopipi <whowhopipi@163.com>
2017-10-06 12:33:48 +0800

    魂晶

---
+++ ExBuddy/Plugins/AtmaHunter/Atma.cs
@@ -0,0 +1,15 @@
+﻿namespace ExBuddy.Plugins.AtmaHunter
+{
+    public class Atma
+    {
+        public string Name { set; get; }
+
+        public string EnName { set; get; }
+
+        public uint Id { set; get; }
+
+        public uint AetheryteId { set; get; }
+
+        public uint ZooId { set; get; }
+    }
+}
---
+++ ExBuddy/Plugins/AtmaHunter/AtmaMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ExBuddy.Plugins.AtmaHunter
+{
+    public class AtmaMap
+    {
+        public static readonly List<Atma> Atmas = new List<Atma>()
+        {
+            new Atma() {Name = "室女之魂晶",EnName = "Atma of the Maiden",Id=7851 ,ZooId=148,AetheryteId = 3},
+            new Atma() {Name = "天蝎之魂晶",EnName = "Atma of the Scorpion",Id=7852 ,ZooId=146,AetheryteId = 19},
+            new Atma() {Name = "金牛之魂晶",EnName = "Atma of the Bull",Id=7855 ,ZooId=145,AetheryteId = 18},
+            new Atma() {Name = "白羊之魂晶",EnName = "Atma of the Ram",Id=7856 ,ZooId=134,AetheryteId = 52},
+            new Atma() {Name = "狮子之魂晶",EnName = "Atma of the Lion",Id=7858 ,ZooId=180,AetheryteId = 16},
+            new Atma() {Name = "双鱼之魂晶",EnName = "Atma of the Fish",Id=7859 ,ZooId=135,AetheryteId = 10},
+            new Atma() {Name = "人马之魂晶",EnName = "Atma of the Archer",Id=7860 ,ZooId=154,AetheryteId = 7},
+            new Atma() {Name = "天秤之魂晶",EnName = "Atma of the Scales",Id=7861 ,ZooId=141,AetheryteId = 53},
+            new Atma() {Name = "双子之魂晶",EnName = "Atma of the Twins",Id=7857 ,ZooId=140,AetheryteId = 17},
+            new Atma() {Name = "巨蟹之魂晶",EnName = "Atma of the Crab",Id=7862 ,ZooId=138,AetheryteId = 14},
+            new Atma() {Name = "摩羯之魂晶",EnName = "Atma of the Goat",Id=7854 ,ZooId=152,AetheryteId = 4},
+            new Atma() {Name = "宝瓶之魂晶",EnName = "Atma of the Water-bearer",Id=7853 ,ZooId=139,AetheryteId = 15}
+        };
+    }
+}
