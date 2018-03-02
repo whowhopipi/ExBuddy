7f2a7d63e192a7c3f32c4dc3512c4d16d42188df
Entrax <entrax.643@gmail.com>
2018-03-02 07:12:27 +0100

    M'naago/Zhloe missing item added to the turn in blacklist

--- ExBuddy/Properties/AssemblyInfo.cs
+++ ExBuddy/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("4.2.*")]
 #pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
-[assembly: AssemblyFileVersion("4.2.2.0")]
+[assembly: AssemblyFileVersion("4.2.2.1")]
 #pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
